c4bb22c60c92cf3dd385e6221b43f9e6e32edb40
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-10-26 17:52:53 -0300

    add: adiciona smart pointer de Game para GameState

--- include/game.hpp
+++ include/game.hpp
@@ -1,7 +1,11 @@
 #ifndef GAME_H
 #define GAME_H
 
+
+#include "gameStates.hpp"
+
 #include <SFML/Graphics.hpp>
+#include <memory>
 
 /**
 * @brief Controla o loop principal do jogo e a janela.
@@ -10,13 +14,15 @@
 */
 class Game {
     sf::RenderWindow window;
+    //SMART POINTER: quando currentState for ser esquecida (saiu do escopo), chama delete para o obj
+    std::unique_ptr<GameState> currentState;
 public:
 //! Inicializa o jogo e cria a janela principal.
     Game();
 //! Executa o loop principal do jogo.
     void run();
 //! Altera o estado atual do jogo (menu, jogar, pausado, etc.).
-    void changeGameState();
+    void changeGameState(std::unique_ptr<GameState> newState);
 };
 
 #endif
