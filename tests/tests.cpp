7b954cdb6028d19b521d08637c50a618bbcbc2fd
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-10-27 11:15:44 -0300

    add: doctest setup

---
+++ tests/tests.cpp
@@ -0,0 +1,43 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include "game.hpp"
+#include "gameStates.hpp"
+#include "resourceManager.hpp"
+#include "entityManager.hpp"
+#include "tilemaps.hpp"
+#include "systemsEntities.hpp"
+
+
+TEST_CASE("Game::run - base cases") {
+    // CHECK(...);
+}
+
+
+TEST_CASE("MenuState:: - ") {
+
+}
+TEST_CASE("PlayState:: - ") {
+
+}
+TEST_CASE("PausedState:: - ") {
+
+}
+
+
+TEST_CASE("EntityManager:: - ") {
+
+}
+
+
+TEST_CASE("TileMap:: - ") {
+
+}
+
+
+TEST_CASE("PhysicsSystem:: - ") {
+    
+}
+TEST_CASE("RenderSystem:: - ") {
+    
+}
