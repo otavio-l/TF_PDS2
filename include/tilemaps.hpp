2878c7a17fe5f5ba6a5c8147256f43b294ae309c
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-10-26 22:18:00 -0300

    refactor: renomeia variáveis do tilemaps

--- include/tilemaps.hpp
+++ include/tilemaps.hpp
@@ -9,18 +9,18 @@
 class TileMap : public sf::Drawable, public sf::Transformable {
     sf::VertexArray m_vertices;
     sf::Texture m_tileset;
-    std::vector<std::vector<int>> m_tiles;
+    std::vector<std::vector<int>> m_matrixTiles;
 
 public:
 /**
 * @brief Carrega um mapa de blocos de um conjunto de blocos e uma matriz de layout 2D.
-* @param tileset Caminho para a imagem do conjunto de blocos.
+* @param fileName Caminho para a imagem do conjunto de blocos.
 * @param tileSize Tamanho de cada bloco.
-* @param tiles Matriz 2D de índices de blocos.
+* @param matrixTiles Matriz 2D de índices de blocos.
 * @return True se o carregamento foi bem-sucedido.
 */
-    bool load(const std::string& tileset, sf::Vector2u tileSize, 
-              const std::vector<std::vector<int>>& tiles);
+    bool load(const std::string& fileName, sf::Vector2u tileSize, 
+              const std::vector<std::vector<int>>& matrixTiles);
 };
 
 #endif
