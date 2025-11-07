67e9f90fa60732fdf1ce6a4dc863013d1bcd2ff5
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-11-06 22:25:47 -0300

    add: adiciona constructor no Game

--- src/game.cpp
+++ src/game.cpp
@@ -1,7 +1,14 @@
 #include "game.hpp"
 
 
+constexpr int g_xPixels = 800;
+constexpr int g_yPixels = 600;
+constexpr int fps = 60;
+
+
 Game::Game() {
+    window.create(sf::VideoMode(g_xPixels, g_xPixels), "Game");
+    window.setFramerateLimit(fps);
     //make_unique is available on c++17 forward
     this->currentState = std::unique_ptr<MenuState>(new MenuState(*this));
 }
