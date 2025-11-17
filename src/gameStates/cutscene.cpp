4ab01eaa98ef63c12139e460e23b7b0281360d27
Otavio L <220527245+otavio-l@users.noreply.github.com>
2025-11-16 21:36:56 -0300

    add: adiciona estado de cutscene

---
+++ src/gameStates/cutscene.cpp
@@ -0,0 +1,89 @@
+#include <string>
+#include <chrono>
+#include <thread>
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include "gameStates.hpp"
+#include "constants.hpp"
+#include "game.hpp"
+
+
+void Cutscene::loadJson(std::string jsonFile) {
+    nlohmann::json cutscene;
+
+    std::ifstream file(jsonFile);
+    if (!file.is_open()) {
+        throw std::runtime_error("Could not open map file");
+    }
+    file >> cutscene;
+    file.close();
+
+    this->spriteFile = cutscene.value("file", "");
+    assert (!cutscene.empty());
+    this->intervalSec = cutscene.value("intervalSec", 0.5f);
+    this->repetitions = cutscene.value("repetitions", 1);
+    this->next = cutscene.value("next", "");
+}
+
+Cutscene::Cutscene(Game &game, std::string jsonFile) 
+: GameState(game), currentFrame(0), dtTotal(0.0f), currentRepetition(1) {
+    loadJson(jsonFile);
+
+    resources.loadTexture(spriteFile);
+    sf::Texture& texture {resources.getTexture(spriteFile)};
+    int width { static_cast<int>( texture.getSize().x ) };
+    assert ((width % constants::xLogicPixels) == 0);
+
+    quantFrame = width / constants::xLogicPixels;
+}
+
+void Cutscene::handleInput(sf::Event& event) {
+    GameState::handleInput(event);
+
+    if (event.type == sf::Event::KeyPressed) {
+        if (event.key.code == sf::Keyboard::Escape) {
+            game.actions.emplace_back(
+                PendingActionType::Push,
+                std::move(std::unique_ptr<PausedState>(new PausedState(game)))
+            );
+        }
+    }
+}
+
+void Cutscene::update(float dt) {
+    if (dtTotal < intervalSec) {
+        dtTotal += dt;
+        return;
+    }
+    dtTotal = 0.0f;
+    if ((currentFrame+1) >= quantFrame) {
+        ++currentRepetition;
+        currentFrame = 0;
+
+        if (currentRepetition > repetitions) {
+            if (next.empty()) {
+                game.actions.emplace_back(
+                    PendingActionType::Pop,
+                    std::move(nullptr)
+                );
+            }
+            else {
+                game.actions.emplace_back(
+                    PendingActionType::Change,
+                    std::move(std::unique_ptr<Cutscene>(new Cutscene(game, next)))
+                );
+            }
+        }
+    }
+    else {
+        ++currentFrame;
+    }
+}
+
+void Cutscene::render(sf::RenderWindow& window) {
+    int x {currentFrame * constants::xLogicPixels};
+    sf::IntRect currentSprite({ x, 0 }, { constants::xLogicPixels, constants::yLogicPixels });
+    sf::Texture& texture {resources.getTexture(spriteFile)};
+    sf::Sprite frame(texture, currentSprite);
+    window.draw(frame);
+}
