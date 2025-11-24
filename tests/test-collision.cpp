36dbbf55ff3a5e97a4d1444fe5e6213a153ed0a7
cauaooa <cauaecbmatos@gmail.com>
2025-11-24 01:11:05 -0300

    add teste de unidade de collision

---
+++ tests/test-collision.cpp
@@ -0,0 +1,50 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include "collision.hpp"
+#include "entity.hpp"
+
+TEST_CASE("checkCollision() base cases") {
+    Entity entityA;
+    entityA.hitbox.setPosition(0.0f, 0.0f);
+    entityA.hitbox.setSize(sf::Vector2f(10.0f, 10.0f));
+
+    Entity entityB;
+    entityB.hitbox.setPosition(5.0f, 5.0f);
+    entityB.hitbox.setSize(sf::Vector2f(10.0f, 10.0f));
+
+    Entity entityC;
+    entityC.hitbox.setPosition(20.0f, 20.0f);
+    entityC.hitbox.setSize(sf::Vector2f(5.0f, 5.0f));
+
+    CHECK(checkCollision(entityA, entityB, 0.0f, 0.0f) == true);
+    CHECK(checkCollision(entityA, entityC, 0.0f, 0.0f) == false);
+}
+
+TEST_CASE("checkCollision() with movement") {
+    Entity entityA;
+    entityA.hitbox.setPosition(0.0f, 0.0f);
+    entityA.hitbox.setSize(sf::Vector2f(10.0f, 10.0f));
+
+    Entity entityB;
+    entityB.hitbox.setPosition(15.0f, 0.0f);
+    entityB.hitbox.setSize(sf::Vector2f(10.0f, 10.0f));
+
+    CHECK(checkCollision(entityA, entityB, 5.0f, 0.0f) == false);
+    CHECK(checkCollision(entityA, entityB, 10.0f, 0.0f) == true);
+    CHECK(checkCollision(entityA, entityB, 20.0f, 0.0f) == true);
+}
+
+TEST_CASE("checkCollision() edge cases") {
+    Entity entityA;
+    entityA.hitbox.setPosition(0.0f, 0.0f);
+    entityA.hitbox.setSize(sf::Vector2f(10.0f, 10.0f));
+
+    Entity entityB;
+    entityB.hitbox.setPosition(10.0f, 0.0f);
+    entityB.hitbox.setSize(sf::Vector2f(10.0f, 10.0f));
+
+    CHECK(checkCollision(entityA, entityB, 0.0f, 0.0f) == false);
+    CHECK(checkCollision(entityA, entityB, 10.0f, 0.0f) == true);
+    CHECK(checkCollision(entityA, entityB, 9.99f, 0.0f) == false);
+}
